17bc400deaa88489e7175d61d2eec977cfd370b2
Rainer Keerdo <eestikaa@gmail.com>
2014-03-11 13:18:35 +0200

    Testing redirect

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,10 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        
+        ServletInputStream inp = req.getInputStream();
+        resp.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        resp.setHeader("Location", "http://www.google.com");
+        resp.sendRedirect("http://www.google.com");
         out.write("Hello PAGE\n".getBytes());
         out.write("This is silly.".getBytes());
         out.flush();
