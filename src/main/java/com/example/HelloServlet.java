7ea34739f9fbfdfffddd8e1beab92717c5e3596e
Rainer Keerdo <eestikaa@gmail.com>
2014-03-16 16:03:38 +0200

    Common database pooling added to dependencies

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletConfig;
@@ -11,11 +13,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 public class HelloServlet extends HttpServlet {
 
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+            throws ServletException, IOException {		
         ServletOutputStream out = resp.getOutputStream();
         ServletInputStream inp = req.getInputStream();
         resp.sendError(9001, "Too good to be displayed");
@@ -25,6 +29,6 @@ public class HelloServlet extends HttpServlet {
         out.write("Hello PAGE\n".getBytes());
         out.flush();
         out.close();
-    }
-    
+	}
+	
 }
