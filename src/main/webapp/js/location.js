faef44200794e67cd1922e423c7cb7396f66b4d4
Joosep Heinmets <joosephe@gmail.com>
2014-03-17 19:12:49 +0200

    county selection testing

---
+++ src/main/webapp/js/location.js
@@ -0,0 +1,70 @@
+function locations() {
+	var selopt = document.getElementById("D1").value;
+	x = document.getElementById("D2");
+	switch (selopt)
+	case 1:
+		x.innerHTML="
+		<option value=\"0\">Vald</option>
+		<option value=\"1\">Aegviidu</option>
+		<option value="2">Anija</option>
+		<option value="3">Harku</option>
+		<option value="4">Jõelähtme</option>
+		<option value="5">Keila</option>
+		<option value="6">Kernu</option>
+		<option value="7">Kiili</option>
+		<option value="8">Kose</option>
+		<option value="9">Kuusalu</option>
+		<option value="10">Nissi</option>
+		<option value="11">Padise</option>
+		<option value="12">Raasiku</option>
+		<option value="13">Rae</option>
+		<option value="14">Saku</option>
+		<option value="15">Saue</option>
+		<option value="16">Vasalemma</option>
+		<option value="17">Viimsi</option>"
+		break;
+	case 2:
+		
+		break;
+	case 3:
+		
+		break;
+	case 4:
+	
+		break;
+	case 5:
+	
+		break;
+	case 6:
+	
+		break;
+	case 7:
+
+		break;
+	case 8:
+
+		break;
+	case 9:
+
+		break;
+	case 10:
+
+		break;
+	case 11:
+
+		break;
+	case 12:
+
+		break;
+	case 13:
+
+		break;
+	case 14:
+
+		break;
+	case 15:
+
+		break;
+	default:
+		
+}
