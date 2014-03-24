8982a46e658b33300078a2943a55901a6a7d60a9
Your Name <kaspar93@msn.com>
2014-03-24 15:51:29 +0200

    The beginning of ultimate CCS sheet!

--- src/main/webapp/js/location.js
+++ src/main/webapp/js/location.js
@@ -4,7 +4,7 @@
 	if(selopt == 1)
 		x.innerHTML="<option value=\"0\">Vald</option><option value=\"1\">Aegviidu</option><option value=\"2\">Anija</option><option value=\"3\">Harku</option><option value=\"4\">Jõelähtme</option><option value=\"5\">Keila</option><option value=\"6\">Kernu</option><option value=\"7\">Kiili</option><option value=\"8\">Kose</option><option value=\"9\">Kuusalu</option><option value=\"10\">Nissi</option><option value=\"11\">Padise</option><option value=\"12\">Raasiku</option><option value=\"13\">Rae</option><option value=\"14\">Saku</option><option value=\"15\">Saue</option><option value=\"16\">Vasalemma</option><option value=\"17\">Viimsi</option>";
 	else if(selopt == 2)
-		x.innerHTML="<option value=\"1\">Emmaste</option><option value=\"2\">Hiiu</option><option value=\"3\">Käina</option><option value=\"4\">Pühalepa</option>";
+		x.innerHTML="<option value=\"0\">Vald</option>value=\"1\">Emmaste</option><option value=\"2\">Hiiu</option><option value=\"3\">Käina</option><option value=\"4\">Pühalepa</option>";
 	else if(selopt == 3)
 		x.innerHTML="<option value=\"0\">Vald</option><option value=\"1\">Alajõe</option><option value=\"2\">Aseri</option><option value=\"3\">Avinurme</option><option value=\"4\">Iisaku</option><option value=\"5\">Illuka</option><option value=\"5\">Jõhvi</option><option value=\"6\">Kohtla</option><option value=\"7\">Kohtla-Nõmme</option><option value=\"8\">Lohusuu</option><option value=\"9\">Lüganuse</option><option value=\"10\">Maidla</option><option value=\"11\">Mäetaguse</option><option value=\"12\">Sonda</option><option value=\"13\">Toila</option><option value=\"14\">Tudulinna</option><option value=\"15\">Vaivara</option>";
 	else if(selopt == 4)
