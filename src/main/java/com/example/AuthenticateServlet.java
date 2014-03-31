3826cbda80ac5b82f002e3fad59ac318ebd7164b
Rainer Keerdo <eestikaa@gmail.com>
2014-03-31 14:19:37 +0300

    Velocity-tools

--- src/main/java/com/example/AuthenticateServlet.java
+++ src/main/java/com/example/AuthenticateServlet.java
@@ -7,9 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AuthenticateServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException{
+import org.apache.velocity.Template;
+import org.apache.velocity.context.Context;
+import org.apache.velocity.servlet.VelocityServlet;
+import org.apache.velocity.tools.view.servlet.VelocityViewServlet;
+
+@SuppressWarnings("deprecation")
+public class AuthenticateServlet extends VelocityViewServlet {
+	public Template handleRequest(HttpServletRequest request, HttpServletResponse response, Context context) {
 		
+		return null;
+
 	}
 }
