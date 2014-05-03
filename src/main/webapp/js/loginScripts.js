2249a07c946d63a8c0a5c47e4c424aec00343cfb
Your Name <kaspar93@msn.com>
2014-05-03 14:28:25 +0300

    Login buttons and modal.

--- src/main/webapp/js/loginScripts.js
+++ src/main/webapp/js/loginScripts.js
@@ -2,3 +2,9 @@ function loginAjax(){
 	
 	
 }
+function loginModal(){
+	el = document.getElementById("loginModal");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+	el2 = document.getElementById("fbButton");
+	el2.style.display = (el2.style.display == "inline") ? "none" : "inline";
+}
