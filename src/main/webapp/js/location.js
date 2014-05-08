536bb1d32020b0682a32cd50e6edd77840f98b47
Your Name <kaspar93@msn.com>
2014-05-08 21:16:56 +0300

    maorfixes

--- src/main/webapp/js/location.js
+++ src/main/webapp/js/location.js
@@ -1,6 +1,6 @@
-﻿function changeLocation() {
-	var selopt = document.getElementById("D1").value;
-	x = document.getElementById("D2");
+﻿function changeLocation(x, y) {
+	var selopt = document.getElementById(x).value;
+	x = document.getElementById(y);
 	if(selopt == 1)
 		x.innerHTML="<option value=\"0\">Vald</option><option value=\"1\">Aegviidu</option><option value=\"2\">Anija</option><option value=\"3\">Harku</option><option value=\"4\">Jõelähtme</option><option value=\"5\">Keila</option><option value=\"6\">Kernu</option><option value=\"7\">Kiili</option><option value=\"8\">Kose</option><option value=\"9\">Kuusalu</option><option value=\"10\">Nissi</option><option value=\"11\">Padise</option><option value=\"12\">Raasiku</option><option value=\"13\">Rae</option><option value=\"14\">Saku</option><option value=\"15\">Saue</option><option value=\"16\">Vasalemma</option><option value=\"17\">Viimsi</option>";
 	else if(selopt == 2)
@@ -34,3 +34,4 @@
 	else
 		x.innerHTML="<option value=\"0\">Vald</option>";
 }
+
